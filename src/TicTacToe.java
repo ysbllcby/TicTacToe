5fcac4e6699add73445cc753813d1b23f5daa5c3
ysbllcby <ysabellacubay@gmail.com>
2023-05-09 12:37:51 -0600

    Methods created.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,31 @@
-public class TicTacToe {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class TicTacToe implements ActionListener {
+
+    public TicTacToe() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
 }
